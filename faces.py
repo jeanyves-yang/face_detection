6fe0bd5acec42bc4a88bd434f0d503dc9bccb2c1
Jean-yves Yang <jean-yves.yang@mastertp>
2015-12-17 11:31:59 +0100

    ENH: now detect only 2 eyes and one mouth per face!

--- faces.py
+++ faces.py
@@ -2,6 +2,7 @@ import cv2
 import cv2.cv as cv
 import sys
 import time
+import numpy as np
 
 def detect(img, cascade):
 	rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3, minSize=(10, 10), flags = cv.CV_HAAR_SCALE_IMAGE)
@@ -25,11 +26,17 @@ if __name__ == '__main__':
 		if (img == None):                                      ## Check for invalid input
 			print "Could not open or find the image"
 		else:
-			cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+			cascade_face = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+			cascade_eye = cv2.CascadeClassifier("haarcascade_eye.xml")
+			cascade_mouth = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
 			gray = cv2.cvtColor(img, cv.CV_BGR2GRAY)
-			gray = cv2.equalizeHist(gray)
+			gray_face = cv2.equalizeHist(gray)
 	
-			rects = detect(gray, cascade)
+			kernel = np.ones((5,5),np.float32)/25
+			gray_face = cv2.filter2D(gray_face,-1,kernel)
+			cv2.imshow('Display face ROI', gray_face)
+
+			rects = detect(gray, cascade_face)
 			
 			print len(rects)
 			
@@ -42,11 +49,50 @@ if __name__ == '__main__':
 			## Extract face ROI
 			faceROI = gray[x1:x2, y1:y2]
 
+			## Detect eyes on the face
+			gray_eyes = cv2.equalizeHist(faceROI)
+			rects_eyes_tmp = detect(gray_eyes, cascade_eye)
+			numrow_eyes = len(rects_eyes_tmp)
+
+			# Keep only the two rectangles at the top of the face
+			rects_eyes_tmp = sorted(rects_eyes_tmp,key=lambda x: x[1])
+			rects_eyes = (rects_eyes_tmp[0],rects_eyes_tmp[1])
+			numrow_eyes = len(rects_eyes)
+
+			## Coordinates base change
+			for i in range(0,numrow_eyes):		
+				rects_eyes[i][1] = rects_eyes[i][1] + x1
+				rects_eyes[i][0] = rects_eyes[i][0] + y1
+				rects_eyes[i][3] = rects_eyes[i][3] + x1
+				rects_eyes[i][2] = rects_eyes[i][2] + y1
+
+			## Detect mouth on the face
+			gray_mouth = cv2.equalizeHist(faceROI)
+			rects_mouth_tmp = detect(gray_mouth, cascade_mouth)
+			numrow_mouth = len(rects_mouth_tmp)
+
+			#Keep only the rectangle at the bottom of the face
+			rects_mouth = sorted(rects_mouth_tmp, key=lambda x:x[3])
+			#rects_mouth = (rects_mouth_tmp[numrow_mouth-1])
+			#numrow_mouth = len(rects_mouth)
+			#print numrow_mouth
+
+			## Coordinates base change	
+			rects_mouth[numrow_mouth-1][1] = rects_mouth[numrow_mouth-1][1] + x1
+			rects_mouth[numrow_mouth-1][0] = rects_mouth[numrow_mouth-1][0] + y1
+			rects_mouth[numrow_mouth-1][3] = rects_mouth[numrow_mouth-1][3] + x1
+			rects_mouth[numrow_mouth-1][2] = rects_mouth[numrow_mouth-1][2] + y1
+			
+			for i in range(0, numrow_mouth-1):
+				del(rects_mouth[0])			
+	
 			## Show face ROI
 			cv2.imshow('Display face ROI', faceROI)
 
 			vis = img.copy()
 			draw_rects(vis, rects, (0, 255, 0))
+			draw_rects(vis, rects_eyes, (255, 0, 0))
+			draw_rects(vis, rects_mouth, (0,0,255))
 
         		cv2.namedWindow('Display image')          ## create window for display
         		cv2.imshow('Display image', vis)          ## Show image in the window
